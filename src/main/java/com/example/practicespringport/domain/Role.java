fead22ceccbbfdffc82afbf18f51bf5a4c28eee0
Vadim Voichenko <vvojchenko0@gmail.com>
2023-09-27 22:41:29 +0300

    Test commit

--- src/main/java/com/example/practicespringport/domain/Role.java
+++ src/main/java/com/example/practicespringport/domain/Role.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     USER,ADMIN;
 
+
     @Override
     public String getAuthority() {
         return name();
